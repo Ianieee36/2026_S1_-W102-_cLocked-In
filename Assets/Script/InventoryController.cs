22300814cc1d65d8e79a586214bff0cf30165b68
ExplosiveSalad <224346505+ExplosiveSalad@users.noreply.github.com>
2026-05-01 02:56:53 +1200

    Inventory with items
    This has placeholder items to show off what the inventory is capable of

--- Assets/Script/InventoryController.cs
+++ Assets/Script/InventoryController.cs
@@ -5,12 +5,19 @@ public class InventoryController : MonoBehaviour
     public GameObject inventoryPanel;
     public GameObject slotPrefab;
     public int slotCount;
+    public GameObject[] itemPrefabs;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         for (int i = 0; i < slotCount; i++)
         {
-            Instantiate(slotPrefab, inventoryPanel.transform);
+           Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+            if(i < itemPrefabs.Length)
+            {
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
         }
     }
 }
